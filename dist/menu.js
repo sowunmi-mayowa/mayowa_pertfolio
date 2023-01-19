dce7df494cab1ce348f370be72272f1eb6d767c7
sowunmi mayowa <smayowa176@gmail.com>
2023-01-19 10:57:36 +0100

    made the moobile nav fade at the page bottom

--- dist/menu.js
+++ dist/menu.js
@@ -5,8 +5,16 @@ menuBtn.addEventListener("click", toggleMenu);
 closeBtn.addEventListener("click", closeMenu);
 
 function toggleMenu() {
-    document.querySelector("#mob-nav").style.display = "block";
+    document.querySelector("#tab-nav").style.display = "block";
 }
 function closeMenu() {
-    document.querySelector("#mob-nav").style.display = "none";
+    document.querySelector("#tab-nav").style.display = "none";
+}
+
+window.onscroll = () => {
+    if(window.innerHeight + window.pageYOffset >= document.body.offsetHeight){
+        document.querySelector("#mob-nav").style.display = "none"
+    }else{
+        document.querySelector("#mob-nav").style.display = "block"
+    }
 }
